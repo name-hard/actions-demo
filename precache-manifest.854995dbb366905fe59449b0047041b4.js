d6b36af7c3cba1ecb15e470e67a58927f96f81d7
name-hard <jokerb010@163.com>
2020-10-16 05:10:36 +0000

    Deploying to gh-pages from  @ 1794e78ed487da2b82554f98d40b24f8ff70fc07 🚀

---
+++ precache-manifest.854995dbb366905fe59449b0047041b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "09f3c3782655d639c673a5ddf7f90f43",
+    "url": "/actions-demo/index.html"
+  },
+  {
+    "revision": "bea2a1d80f576938ef5a",
+    "url": "/actions-demo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0174fd6496658de5541d",
+    "url": "/actions-demo/static/js/2.9745be95.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/actions-demo/static/js/2.9745be95.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bea2a1d80f576938ef5a",
+    "url": "/actions-demo/static/js/main.20089116.chunk.js"
+  },
+  {
+    "revision": "a81af805784126952ea1",
+    "url": "/actions-demo/static/js/runtime-main.23801087.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/actions-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
